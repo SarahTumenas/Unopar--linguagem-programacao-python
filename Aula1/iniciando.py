4a33de26247d1dd3aec22dafce5e2597fec267a6
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-03-12 11:52:22 -0300

    Variáveis e tipos básicos de dados em Python

--- Aula1/iniciando.py
+++ Aula1/iniciando.py
@@ -1,2 +1,10 @@
 nome = input("Digite um nome: ")
 print("Olá %s, bem vindo a disciplina de programação. parabéns pelo seu primeiro hello world!" % (nome))
+
+# função format
+
+print("Olá {}, bem vindo a disciplina de Programação. Parabéns pelo seu Hello world!".format(nome))
+
+# strings formatadas
+
+print(f"Olá {nome},  bem vindo a disciplina de Programação. Parabéns pelo seu Hello world!")
