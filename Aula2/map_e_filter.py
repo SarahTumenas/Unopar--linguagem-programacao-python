509f038c4548be484196c40b60a7a7faf6f5528b
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-03-19 11:45:36 -0300

    Pares e ímpares

--- Aula2/map_e_filter.py
+++ Aula2/map_e_filter.py
@@ -30,4 +30,9 @@ numeros = list(range(0, 21))
 
 numeros_pares = list(filter(lambda x: x % 2 == 0, numeros))
 
-print(numeros_pares)
+print(f"Números pares = {numeros_pares}")
+
+numeros_impares = list(filter(lambda x: x % 2 != 0, numeros))
+print(f"Números ímpares = {numeros_impares}")
+
+
