b46f787e4fee47b9c8ed46f9ac192471db42ab8d
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-03-26 11:01:36 -0300

    busca binaria

--- Aula2/busca_binaria.py
+++ Aula2/busca_binaria.py
@@ -1,18 +1,23 @@
-
 def executar_busca_binaria(lista, valor):
-
     minimo = 0
 
     maximo = len(lista) - 1
 
     while minimo <= maximo:
-     meio = (minimo + maximo) // 2 #Encontra o elemento que divide a lista ao meio
-# Verifica se o valor procurado está a esquerda ou direitado valor central
+        meio = (minimo + maximo) // 2  # Encontra o elemento que divide a lista ao meio
+    # Verifica se o valor procurado está a esquerda ou direitado valor central
     if valor < lista[meio]:
         maximo = meio - 1
     elif valor > lista[meio]:
-           minimo = meio + 1
+        minimo = meio + 1
     else:
         return True
     return False
 
+
+lista = list(range(1, 50))
+
+print(lista)
+
+print('\n', executar_busca_binaria(lista=lista, valor=10))
+print('\n', executar_busca_binaria(lista=lista, valor=20))
