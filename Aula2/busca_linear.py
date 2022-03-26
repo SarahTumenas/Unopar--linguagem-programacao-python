053574785616f7e08a96535bb5e061a1c3afb558
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-03-26 10:26:28 -0300

    busca sequencial

---
+++ Aula2/busca_linear.py
@@ -0,0 +1,14 @@
+# Busca linear (ou Busca Sequencial)
+# Percorre os elementos da sequência procurando aquele de destino, começa por uma das extremidades da sequência e vai percorrendo até encontrar (ou não) o valordesejado.
+# Pesquisa linear examina todos os elementos da sequência até encontrar o de destino, o que pode ser muito custoso computacionalmente.
+
+
+def executar_busca_linear(lista, valor):
+    for elemento in lista:
+        if valor == elemento: return True
+    return False
+
+
+vogais = 'aeiou'
+resultado = vogais.index('e')
+print(resultado)
